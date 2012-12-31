9623f637ba12dab953bf72c9e81c1598de3388b3
fdavis <reddot357@hotmail.com>
2012-12-30 21:43:30 -0800

    ntp/nx alright, working toward kerb5
    strange error, when bringing up all servers, "inst id not found"
    it would be better to iterate through raising all servers
    then iterating through waiting for them to get live

--- launch-and-configure.sh
+++ launch-and-configure.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# quit script on cmd fail
+set -e
+
 ansible-playbook bring-up/setup.yml -i inv/default
 ansible-playbook all-servers/configure-fireball.yml
 ansible-playbook ntp-servers/configure-ntp-servers.yml
+ansible-playbook all-servers/configure-ntpd.yml
 ansible-playbook nx-servers/configure-nx-servers.yml
