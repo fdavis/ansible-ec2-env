7b38f087e4f7c8541c2e7ec9c94ef7fafdeb55d6
fdavis <reddot357@hotmail.com>
2013-02-09 15:17:38 -0800

    HAPROXY worked!!!

--- configure.sh
+++ configure.sh
@@ -3,6 +3,4 @@
 # quit script on cmd fail
 set -e
 
-ansible-playbook -v all-servers/configure-standard-pkgs.yml
-ansible-playbook -v nx-servers/configure-nx-servers.yml
-ansible-playbook -v c9-servers/configure-c9-servers.yml
+ansible-playbook -v configure-all.yml
